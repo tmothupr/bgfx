34d791fd341a32b4da47eb17936f6f5fc96ab138
Hugo Amnov <hugo.amiard@laposte.net>
2020-02-02 21:32:00 +0100

    Add WebGPU 12-lod example

--- examples/12-lod/lod.cpp
+++ examples/12-lod/lod.cpp
@@ -9,6 +9,8 @@
 
 #include <bx/readerwriter.h>
 
+#define WEBGPU 1
+
 namespace
 {
 
@@ -44,7 +46,7 @@ public:
 		m_reset  = BGFX_RESET_VSYNC;
 
 		bgfx::Init init;
-		init.type     = args.m_type;
+        init.type     = WEBGPU ? bgfx::RendererType::WebGPU : args.m_type;
 		init.vendorId = args.m_pciId;
 		init.resolution.width  = m_width;
 		init.resolution.height = m_height;
