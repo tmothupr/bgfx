22bf457db300c0742a532c2f942cf1b74fd71ac6
Hugo Amnov <hugo.amiard@laposte.net>
2020-02-02 23:28:38 +0100

    Add WebGPU 32-particles example

--- examples/32-particles/particles.cpp
+++ examples/32-particles/particles.cpp
@@ -17,6 +17,8 @@
 
 #include <ps/particle_system.h>
 
+#define WEBGPU 1
+
 namespace
 {
 
@@ -244,7 +246,7 @@ public:
 		m_reset  = BGFX_RESET_VSYNC;
 
 		bgfx::Init init;
-		init.type     = args.m_type;
+        init.type     = WEBGPU ? bgfx::RendererType::WebGPU : args.m_type;
 		init.vendorId = args.m_pciId;
 		init.resolution.width  = m_width;
 		init.resolution.height = m_height;
