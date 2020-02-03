c4d38cbb4270a9a0ef041cc3a924dcdf918aff6c
Hugo Amnov <hugo.amiard@laposte.net>
2020-02-04 00:33:04 +0100

    Add WebGPU 40-svt example (not working

--- examples/40-svt/svt.cpp
+++ examples/40-svt/svt.cpp
@@ -20,6 +20,8 @@
 #include "bounds.h"
 #include "vt.h"
 
+#define WEBGPU 1
+
 namespace
 {
 
@@ -79,8 +81,7 @@ public:
 		m_reset = BGFX_RESET_VSYNC;
 
 		bgfx::Init init;
-
-		init.type = args.m_type;
+        init.type     = WEBGPU ? bgfx::RendererType::WebGPU : args.m_type;
 		init.vendorId = args.m_pciId;
 		init.resolution.width = m_width;
 		init.resolution.height = m_height;
